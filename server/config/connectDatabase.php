fdb3315ba5d771e61ee0627580477fc2ca4a0651
Shiron <vannguyen11a21@gmail.com>
2025-10-18 16:37:15 +0700

    create file connect database

---
+++ server/config/connectDatabase.php
@@ -0,0 +1,8 @@
+<?php 
+$connect = mysqli_connect('localhost', 'root', 'Nguyen31072006!', 'sign_in');
+//Nếu có lỗi xảy ra thì dừng đoạn mã và in thông báo lỗi
+if(!$connect){
+     die("Error: Could not connect to the database. An error" . mysqli_connect_error(). "ocurred.");
+}
+mysqli_set_charset($connect,'utf8')
+?>
