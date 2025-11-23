e79961ad04eb58e0e8aeacf93c9cd41aea153813
Shiron <vannguyen11a21@gmail.com>
2025-11-23 11:37:47 +0700

    Luu tam code hien tai de quay ve qua khu

--- client/src/components/TaskCard/updateTask.jsx
+++ client/src/components/TaskCard/updateTask.jsx
@@ -67,89 +67,91 @@ export const UpdateTask = ({ taskData, onClose }) => {
   //onClick={(e) => e.stopPropagation()} vào cái Form (lớp bên trong).
   // Tại sao? Để khi bạn click vào ô input để gõ chữ, sự kiện click đó không "nổi bong bóng" (bubble) ra ngoài Overlay, tránh việc đang gõ thì form bị đóng.
   return (
-    <div className="modal-overlay-for-update" onClick={onClose}>
-      <div className="form-update" onClick={(e) => e.stopPropagation()}>
-        <div className="content-top">
-          <h1 id="update-title-text">Update your task</h1>
-          <i className="fa-solid fa-pen"></i>
-        </div>
-        <div className="content-body">
-          <div className="update-input-task-name">
-            <label htmlFor="update-input-name">Task Name: </label>
-            <input
-              type="text"
-              name="titleTask"
-              id="update-input-name"
-              placeholder="Update your name task"
-              value={formData.titleTask}
-              onChange={handleChange}
-            />
+    <div className="SCOPE_FORM_UPDATE">
+      <div className="modal-overlay-for-update" onClick={onClose}>
+        <div className="form-update" onClick={(e) => e.stopPropagation()}>
+          <div className="content-top">
+            <h1 id="update-title-text">Update your task</h1>
+            <i className="fa-solid fa-pen"></i>
           </div>
-          <div className="update-sub-task">
-            <label htmlFor="sub-task">Sub tasks: </label>
-            {formData.detailTask && formData.detailTask.length > 0 ? (
-              <div className="sub-tasks-list">
-                {formData.detailTask.map((item, index) => (
-                  <div className="sub-tasks-item" key={item.idTask || index}>
-                    <input
-                      type="text"
-                      name=""
-                      id=""
-                      value={item.title}
-                      onChange={(e) =>
-                        handleSubTasksChange(index, e.target.value)
-                      }
-                    />
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p style={{ color: "#888", fontStyle: "italic" }}>
-                Không có subtask nào
-              </p>
-            )}
-          </div>
-
-          <div className="update-category-and-deadline">
-            <div className="update-select-wrapper">
-              <select
-                className="update-category-select"
-                name="categoryTask"
-                value={formData.categoryTask}
+          <div className="content-body">
+            <div className="update-input-task-name">
+              <label htmlFor="update-input-name">Task Name: </label>
+              <input
+                type="text"
+                name="titleTask"
+                id="update-input-name"
+                placeholder="Update your name task"
+                value={formData.titleTask}
                 onChange={handleChange}
-              >
-                <option value="" disabled selected>
-                  Category
-                </option>
-                <option value="work">Work</option>
-                <option value="personal">Personal</option>
-                <option value="study">Study</option>
-              </select>
-              <i className="fa-solid fa-chevron-down"></i>
+              />
+            </div>
+            <div className="update-sub-task">
+              <label htmlFor="sub-task">Sub tasks: </label>
+              {formData.detailTask && formData.detailTask.length > 0 ? (
+                <div className="sub-tasks-list">
+                  {formData.detailTask.map((item, index) => (
+                    <div className="sub-tasks-item" key={item.idTask || index}>
+                      <input
+                        type="text"
+                        name=""
+                        id=""
+                        value={item.title}
+                        onChange={(e) =>
+                          handleSubTasksChange(index, e.target.value)
+                        }
+                      />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p style={{ color: "#888", fontStyle: "italic" }}>
+                  Không có subtask nào
+                </p>
+              )}
             </div>
 
-            <div className="update-datepicker-wrapper">
-              <p id="update-deadline-text">Deadline:</p>
-              <DatePicker
-                selected={formData.deadlineTask}
-                onChange={handleDateChange}
-                placeholderText="Click to select date"
-                showTimeSelect
-                dateFormat="dd/MM/yyyy HH:mm"
-                minDate={new Date()} // Không cho chọn ngày trong quá khứ
-                isClearable // Hiển thị nút (x) để xóa ngày đã chọn
-                // Chỉ hiển thị lịch khi bấm vào icon
-                showIcon
-                icon="fa fa-calendar" // Cần cài thêm Font Awesome nếu dùng
-                // Hiển thị tháng và năm để chọn nhanh
-                showYearDropdown
-                showMonthDropdown
-                dropdownMode="select"
-              ></DatePicker>
+            <div className="update-category-and-deadline">
+              <div className="update-select-wrapper">
+                <select
+                  className="update-category-select"
+                  name="categoryTask"
+                  value={formData.categoryTask}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled selected>
+                    Category
+                  </option>
+                  <option value="work">Work</option>
+                  <option value="personal">Personal</option>
+                  <option value="study">Study</option>
+                </select>
+                <i className="fa-solid fa-chevron-down"></i>
+              </div>
+
+              <div className="update-datepicker-wrapper">
+                <p id="update-deadline-text">Deadline:</p>
+                <DatePicker
+                  selected={formData.deadlineTask}
+                  onChange={handleDateChange}
+                  placeholderText="Click to select date"
+                  showTimeSelect
+                  dateFormat="dd/MM/yyyy HH:mm"
+                  minDate={new Date()} // Không cho chọn ngày trong quá khứ
+                  isClearable // Hiển thị nút (x) để xóa ngày đã chọn
+                  // Chỉ hiển thị lịch khi bấm vào icon
+                  showIcon
+                  icon="fa fa-calendar" // Cần cài thêm Font Awesome nếu dùng
+                  // Hiển thị tháng và năm để chọn nhanh
+                  showYearDropdown
+                  showMonthDropdown
+                  dropdownMode="select"
+                ></DatePicker>
+              </div>
             </div>
-          </div>
-          <div className="btn-group">
-            <Button>Update</Button>
+            <div className="btn-group">
+              <Button>Update</Button>
+            </div>
           </div>
         </div>
       </div>
